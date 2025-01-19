c74b4ce4caab34c9e9d5fef3c33bfaded712c814
k022g0114@g.neec.ac.jp <k022g0114@g.neec.ac.jp>
2025-01-19 22:55:06 +0900

    save

--- main.cpp
+++ main.cpp
@@ -41,60 +41,125 @@ static SOCKET CreateSocket() {
 	return mySocket;
 }
 
-// ソケットに名前を付けて接続
-static bool BindAndConnectSocket(SOCKET sockfd, unsigned short portNumber) {
-	sockaddr_in server_addr{};
-	memset(&server_addr, 0, sizeof(SOCKADDR_IN));
+// サーバーアドレスを設定する関数
+static void SetServerAddress(sockaddr_in& server_addr, unsigned short portNumber) {
+	memset(&server_addr, 0, sizeof(sockaddr_in));
 	server_addr.sin_family = AF_INET;
 	server_addr.sin_port = htons(portNumber);
 	server_addr.sin_addr.s_addr = INADDR_ANY;
+}
 
-	// sockaddr_inをsockaddrにキャストしてconnect関数に渡す
-	if (connect(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
-		printf("接続に失敗しました\n");
+// ソケットをバインドする関数
+static bool BindSocket(SOCKET sockfd, const sockaddr_in& server_addr) {
+	if (bind(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr)) == SOCKET_ERROR) {
+		printf("バインドに失敗しました。エラーコード: %d\n", WSAGetLastError());
 		return false;
 	}
 	return true;
 }
 
+// ソケットをリッスン状態にする関数
+static bool ListenSocket(SOCKET sockfd) {
+	if (listen(sockfd, SOMAXCONN) == SOCKET_ERROR) {
+		printf("リッスンに失敗しました。エラーコード: %d\n", WSAGetLastError());
+		return false;
+	}
+	return true;
+}
+
+// ソケットに名前を付けて接続
+static bool BindAndConnectSocket(SOCKET sockfd, unsigned short portNumber) {
+	sockaddr_in server_addr{};
+	SetServerAddress(server_addr, portNumber);
+
+	if (!BindSocket(sockfd, server_addr)) {
+		return false;
+	}
+
+	if (!ListenSocket(sockfd)) {
+		return false;
+	}
+
+	return true;
+}
+
+// クライアントからの接続を受け入れる
+static SOCKET AcceptClientConnection(SOCKET listenSock) {
+	sockaddr_in client_addr{};
+	int client_addr_size = sizeof(client_addr);
+
+	// クライアントからの接続を受け入れる
+	SOCKET clientSock = accept(listenSock, (struct sockaddr*)&client_addr, &client_addr_size);
+	if (clientSock == INVALID_SOCKET) {
+		printf("クライアントの接続受け入れに失敗しました。エラーコード: %d\n", WSAGetLastError());
+		return INVALID_SOCKET;
+	}
+
+	printf("クライアントが接続しました\n");
+	return clientSock;
+}
+
+
 
 
 int main()
 {
-	WSADATA wsaData{};
-
-	/* WinSock初期化 */
-	if (WSAStartup(MAKEWORD(2, 0), &wsaData) != 0)
-	{
-		// 初期化エラー
-		printf("WinSockの初期化に失敗しました\n");
-		return FALSE;
+	WSADATA wsaData;
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+		printf("WSAStartupに失敗しました。エラーコード: %d\n", WSAGetLastError());
+		return 1;
 	}
 
-	/* ポート番号の設定 */
-	unsigned short portNumber = Get_PortNumber();
+	SOCKET sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sockfd == INVALID_SOCKET) {
+		printf("ソケットの作成に失敗しました。エラーコード: %d\n", WSAGetLastError());
+		WSACleanup();
+		return 1;
+	}
 
-	/* ソケット作成 */
-	SOCKET sockfd = CreateSocket();
-
-	/* ソケットに名前を付けて接続 */
+	unsigned short portNumber = 8080;
 	if (!BindAndConnectSocket(sockfd, portNumber)) {
 		closesocket(sockfd);
 		WSACleanup();
-		return FALSE;
+		return 1;
+	}
+
+	printf("サーバーがポート %d でリッスンを開始しました。\n", portNumber);
+
+
+	while (1) {
+		SOCKET clientSock = AcceptClientConnection(sockfd);
+		if (clientSock == INVALID_SOCKET) {
+			continue;
+		}
+
+		while (1) {
+			int nRcv = 0;
+			char szBuf[1024]{};
+
+			nRcv = recv(clientSock, szBuf, sizeof(szBuf) - 1, 0);
+			if (nRcv == SOCKET_ERROR) {
+				printf("データ受信に失敗しました。エラーコード: %d\n", WSAGetLastError());
+				closesocket(clientSock);
+				break;
+			}
+			else if (nRcv == 0) {
+				printf("クライアントが接続を閉じました。\n");
+				closesocket(clientSock);
+				break;
+			}
+
+			szBuf[nRcv] = '\0'; // 受信データを文字列として扱うために終端文字を追加
+			printf("受信データ: %s\n", szBuf);
+
+			// 受信データに対する処理をここに追加
+		}
 	}
 
 
-
-
-
-	/* ソケットを閉じる */
 	closesocket(sockfd);
-
-	/* WinSock終了 */
 	WSACleanup();
-
-	return TRUE;
+	return 0;
 }
 
 
